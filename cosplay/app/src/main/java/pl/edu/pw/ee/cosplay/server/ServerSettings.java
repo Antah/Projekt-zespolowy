cccba374ee906003e557b3f7a23279910c144fc6
trzye <shafear100@gmail.com>
2016-01-07 16:43:12 +0100

    fix na gradle

--- cosplay/app/src/main/java/pl/edu/pw/ee/cosplay/server/ServerSettings.java
+++ cosplay/app/src/main/java/pl/edu/pw/ee/cosplay/server/ServerSettings.java
@@ -4,5 +4,5 @@ package pl.edu.pw.ee.cosplay.server;
  * Created by Michał on 2015-12-24.
  */
 public class ServerSettings {
-    public final static String SERVER_URL="http://192.168.43.132:8080";
+    public final static String SERVER_URL="http://192.168.43.128:8080";
 }
