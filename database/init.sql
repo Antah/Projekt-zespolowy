202fcf218936fc0625f1e00a2ce6d37fbe6b394c
trzye <shafear100@gmail.com>
2016-05-14 20:07:32 +0200

    baza danych

--- database/init.sql
+++ database/init.sql
@@ -1,26 +1,87 @@
-CREATE DATABASE IF NOT EXISTS cosplay;
+drop table if exists rating;
+drop table if exists comment;
+drop table if exists characteer;
+drop table if exists franchise;
+drop table if exists photo;
+drop table if exists observation;
+drop table if exists user;
+drop table if exists binary_photo;
 
-use cosplay;
-
-drop table if exists users;
-drop table if exists status;
-
-CREATE TABLE status(
-  status_id INT NOT NULL,
-  name VARCHAR(16) NOT NULL,
-  PRIMARY KEY (status_id)
+create table binary_photo(
+	binary_photo_id int primary key,
+    binary_data binary
 );
 
-insert into status values(0, 'DELETED');
-insert into status values(1, 'NORMAL');
 
-CREATE TABLE users(
-  user_id INT AUTO_INCREMENT,
-  login VARCHAR(16) NOT NULL UNIQUE, 
-  password VARCHAR(256) NOT NULL,
-  status_id INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (user_id),
-  FOREIGN KEY (status_id) REFERENCES status(status_id)
+
+create table user(
+	username nvarchar(16) primary key,
+    passwd nvarchar(512),
+    avatar_binary_photo_id int,
+	foreign key (avatar_binary_photo_id) references binary_photo(binary_photo_id)
 );
 
-insert into users(login,password) values('test','test');
+
+
+create table observation(
+	observation_id int primary key,
+    observed nvarchar(16),
+    observer nvarchar(16),
+	foreign key (observed) references user(username),
+    foreign key (observer) references user(username)
+);
+
+
+
+create table photo(
+	photo_id int primary key,
+    photo_binary_photo_id int,
+    username nvarchar(16),
+    upload_date date,
+    description nvarchar(512),
+	foreign key (username) references user(username),
+	foreign key (photo_binary_photo_id) references binary_photo(binary_photo_id)
+);
+
+
+
+create table franchise(
+	franchise_id int primary key,
+    photo_id int,
+    franchise_name nvarchar(128),
+	foreign key (photo_id) references photo(photo_id)
+);
+
+
+
+create table characteer(
+	characteer_id int primary key,
+    photo_id int,
+    characteer_name nvarchar(128),
+	foreign key (photo_id) references photo(photo_id)
+);
+
+
+
+create table comment(
+	comment_id int primary key,
+    content nvarchar(256),
+    username nvarchar(16),
+    comment_date date,
+    photo_id int,
+	foreign key (photo_id) references photo(photo_id),
+	foreign key (username) references user(username)
+);
+
+
+
+create table rating(
+	rating_id int primary key,
+    username nvarchar(16),
+	photo_id int,
+    rating_similarity int,
+	rating_quality int,
+    rating_arrangemnt int,
+	foreign key (photo_id) references photo(photo_id),
+	foreign key (username) references user(username)
+);
