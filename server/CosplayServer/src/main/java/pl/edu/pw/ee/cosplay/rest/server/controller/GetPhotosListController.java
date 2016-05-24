1ec48937f987d232c28662d7f1b78c1306fc21f9
trzye <shafear100@gmail.com>
2016-05-24 08:45:28 +0200

    comm

--- server/CosplayServer/src/main/java/pl/edu/pw/ee/cosplay/rest/server/controller/GetPhotosListController.java
+++ server/CosplayServer/src/main/java/pl/edu/pw/ee/cosplay/rest/server/controller/GetPhotosListController.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.ee.cosplay.rest.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.SerializationUtils;
@@ -8,11 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pw.ee.cosplay.rest.model.constants.UrlData;
+import pl.edu.pw.ee.cosplay.rest.model.controller.photos.RatingData;
 import pl.edu.pw.ee.cosplay.rest.model.controller.photos.SimplePhotoData;
 import pl.edu.pw.ee.cosplay.rest.model.controller.photos.getphotoslist.GetPhotosListInput;
 import pl.edu.pw.ee.cosplay.rest.model.controller.photos.getphotoslist.GetPhotosListOutput;
+import pl.edu.pw.ee.cosplay.rest.model.entity.McPhotoEntity;
+import pl.edu.pw.ee.cosplay.rest.server.dao.BinaryPhotoDAO;
+import pl.edu.pw.ee.cosplay.rest.server.dao.PhotoDAO;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 @RestController()
 @RequestMapping(UrlData.GET_PHOTOS_LIST_PATH)
@@ -24,15 +31,52 @@ public class GetPhotosListController {
         GetPhotosListOutput output = new GetPhotosListOutput();
 
         //TODO implementacja
-        mockOutput(output);
+        mockOutput(input, output);
 
         byte[] byteOutput = SerializationUtils.serialize(output);
         return new ResponseEntity<>(byteOutput,HttpStatus.OK);
     }
 
-    private void mockOutput(GetPhotosListOutput output) {
+
+    @Autowired
+    PhotoDAO photoDAO;
+
+    @Autowired
+    BinaryPhotoDAO binaryPhotoDAO;
+
+    private void mockOutput(GetPhotosListInput input, GetPhotosListOutput output) {
         output.setAreThereNextPhotos(false);
         ArrayList<SimplePhotoData> simplePhotoDataList = new ArrayList<>();
+
+        Iterable<McPhotoEntity> photoList = photoDAO.findAll();
+
+        for(McPhotoEntity photo : photoList){
+            SimplePhotoData simplePhoto = new SimplePhotoData();
+            simplePhoto.setFranchisesList(new HashSet<String>());
+            simplePhoto.setCharactersList(new HashSet<String>());
+            simplePhoto.setCommentsNumber(0);
+            simplePhoto.setPhotoBinaryData(
+                    binaryPhotoDAO.findOne(
+                            photo.getPhotoId()
+                    ).getBinaryData()
+            );
+            simplePhoto.setAvatarBinaryData(
+                    binaryPhotoDAO.findOne(
+                            photo.getPhotoId()
+                    ).getBinaryData()
+            );
+            simplePhoto.setUsername(photo.getUsername());
+            simplePhoto.setUploadDate(photo.getUploadDate());
+            RatingData ratingData = new RatingData();
+            ratingData.setGeneralRate(5);
+            ratingData.setArrangementRate(5);
+            ratingData.setQualityRate(5);
+            ratingData.setSimilarityRate(5);
+            simplePhoto.setRatingData(ratingData);
+
+            simplePhotoDataList.add(simplePhoto);
+        }
+
         output.setSimplePhotoDataList(simplePhotoDataList);
     }
 
