d1a7fc0fc17a1e673c0601aa8170db26dbdaf008
trzye <shafear100@gmail.com>
2016-06-05 22:36:29 +0200

    zmiana gitignore i porządki

--- server/CosplayServer/src/main/java/pl/edu/pw/ee/cosplay/rest/server/controller/AddCommentController.java
+++ server/CosplayServer/src/main/java/pl/edu/pw/ee/cosplay/rest/server/controller/AddCommentController.java
@@ -1,6 +1,5 @@
 package pl.edu.pw.ee.cosplay.rest.server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.SerializationUtils;
@@ -12,25 +11,16 @@ import pl.edu.pw.ee.cosplay.rest.model.constants.ErrorMessage;
 import pl.edu.pw.ee.cosplay.rest.model.constants.UrlData;
 import pl.edu.pw.ee.cosplay.rest.model.controller.photos.addcomment.AddCommentInput;
 import pl.edu.pw.ee.cosplay.rest.model.controller.photos.addcomment.AddCommentOutput;
-import pl.edu.pw.ee.cosplay.rest.model.controller.photos.addphoto.AddPhotoInput;
-import pl.edu.pw.ee.cosplay.rest.model.controller.photos.addphoto.AddPhotoOutput;
-import pl.edu.pw.ee.cosplay.rest.model.entity.McBinaryPhotoEntity;
-import pl.edu.pw.ee.cosplay.rest.model.entity.McCharacteerEntity;
-import pl.edu.pw.ee.cosplay.rest.model.entity.McFranchiseEntity;
-import pl.edu.pw.ee.cosplay.rest.model.entity.McPhotoEntity;
-import pl.edu.pw.ee.cosplay.rest.server.dao.*;
 import pl.edu.pw.ee.cosplay.rest.server.security.LoggedUsers;
 
-import java.sql.Date;
-
 @RestController()
-@RequestMapping(UrlData.ADD_PHOTO_PATH)
+@RequestMapping(UrlData.ADD_COMMENT_CONTROLLER)
 public class AddCommentController {
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<?> addComment(@RequestBody byte[] byteInput) {
         AddCommentInput input = (AddCommentInput) SerializationUtils.deserialize(byteInput);
-        if(LoggedUsers.isLogged(input.getAuthenticationData())){
+        if (LoggedUsers.isLogged(input.getAuthenticationData())) {
 
             AddCommentOutput output = new AddCommentOutput();
 
@@ -38,7 +28,7 @@ public class AddCommentController {
             mockOutput(input, output);
 
             byte[] byteOutput = SerializationUtils.serialize(output);
-            return new ResponseEntity<>(byteOutput,HttpStatus.OK);
+            return new ResponseEntity<>(byteOutput, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(ErrorMessage.NOT_LOGGED, HttpStatus.UNAUTHORIZED);
         }
